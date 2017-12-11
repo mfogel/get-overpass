b362e77d678ac1189dad335bb684757e72e38b89
Mike Fogel <mike@fogel.ca>
2017-12-11 19:53:41 -0300

    Adopt auto-formatting from prettier-standard

--- cli.js
+++ cli.js
@@ -5,17 +5,15 @@ const pjson = require('./package.json')
 const process = require('process')
 
 const argv = require('yargs')
-  .usage(
-    '$0 <OSM ID>',
-    pjson['description'],
-    (yargs) => {
-      yargs.positional('OSM ID', {
-        describe: 'An OSM ID in either OSMtoGeoJSON or Mapbox form.\n' +
-                  'OSMtoGeoJSON: {OSM type}/{ID of OSM object of that type}\n' +
-                  'Mapbox: {OSM node ID}*10 / {OSM way ID}*10+1 / {OSM relation ID}*10+4',
-        type: 'string'
-      })
+  .usage('$0 <OSM ID>', pjson['description'], yargs => {
+    yargs.positional('OSM ID', {
+      describe:
+        'An OSM ID in either OSMtoGeoJSON or Mapbox form.\n' +
+        'OSMtoGeoJSON: {OSM type}/{ID of OSM object of that type}\n' +
+        'Mapbox: {OSM node ID}*10 / {OSM way ID}*10+1 / {OSM relation ID}*10+4',
+      type: 'string'
     })
+  })
   .option('m', {
     alias: 'mapbox-ids',
     desc: 'Output OSM IDs in Mapbox format',
@@ -29,12 +27,15 @@ const argv = require('yargs')
   })
   .alias('h', 'help')
   .alias('v', 'version')
-  .strict()
-  .argv
+  .strict().argv
 
 async function main () {
   try {
-    const data = await getOverpass(argv['OSMID'], argv.mapboxIds, argv.apiEndpoint)
+    const data = await getOverpass(
+      argv['OSMID'],
+      argv.mapboxIds,
+      argv.apiEndpoint
+    )
     process.stdout.write(JSON.stringify(data))
   } catch (e) {
     console.error(e)
--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function osmtogeojson2mapbox (osmtogeojsonId) {
   if (osmType === 'relation') return osmTypeId * 10 + 4
   if (osmType === 'way') return osmTypeId * 10 + 1
   if (osmType === 'node') return osmTypeId * 10
-  throw new Error(`Unable to convert OSMtoGeoJSON ID '${osmtogeojsonId}' to Mapbox ID`)
+  throw new Error(
+    `Unable to convert OSMtoGeoJSON ID '${osmtogeojsonId}' to Mapbox ID`
+  )
 }
 
 function mapbox2osmtogeojson (mapboxId) {
@@ -16,27 +18,43 @@ function mapbox2osmtogeojson (mapboxId) {
   if (lastDigit === 4) return `relation/${osmTypeId}`
   if (lastDigit === 1) return `way/${osmTypeId}`
   if (lastDigit === 0) return `node/${osmTypeId}`
-  throw new Error(`Unable to convert Mapbox ID '${mapboxId}' to OSMtoGeoJSON ID`)
+  throw new Error(
+    `Unable to convert Mapbox ID '${mapboxId}' to OSMtoGeoJSON ID`
+  )
 }
 
 function getQuery (osmtogeojsonId) {
   const [osmType, osmTypeId] = osmtogeojsonId.split('/')
-  if (osmType === 'relation') return `(relation(${osmTypeId});way(r);node(w););out;`
+  if (osmType === 'relation') {
+    return `(relation(${osmTypeId});way(r);node(w););out;`
+  }
   if (osmType === 'way') return `((way(${osmTypeId});node(w););out;`
   if (osmType === 'node') return `node(${osmTypeId});out;`
   throw new Error(`Unrecognized OSM type ${osmType}`)
 }
 
-module.exports = function (osmId, mapboxIds = defaults['mapbox-ids'], apiEndpoint = defaults['api-endpoint']) {
+module.exports = function (
+  osmId,
+  mapboxIds = defaults['mapbox-ids'],
+  apiEndpoint = defaults['api-endpoint']
+) {
   if (!String(osmId).includes('/')) osmId = mapbox2osmtogeojson(osmId)
   const query = getQuery(osmId)
-  const translatedOpts = {overpassUrl: apiEndpoint, flatProperties: true}
+  const translatedOpts = { overpassUrl: apiEndpoint, flatProperties: true }
   return new Promise(function (resolve, reject) {
     function callback (error, data) {
       if (error !== undefined) return reject(new Error(error.message))
-      if (data.features.length === 0) return reject(new Error(`${osmId} not found`))
+      if (data.features.length === 0) {
+        return reject(new Error(`${osmId} not found`))
+      }
       const feature = data.features.find(f => f.id === osmId)
-      if (feature === undefined) return reject(new Error(`Found ${osmId} but did not recieve in payload. Bad data in OSM?`))
+      if (feature === undefined) {
+        return reject(
+          new Error(
+            `Found ${osmId} but did not recieve in payload. Bad data in OSM?`
+          )
+        )
+      }
       if (mapboxIds) {
         feature.id = osmtogeojson2mapbox(feature.id)
         feature.properties.id = osmtogeojson2mapbox(feature.properties.id)
